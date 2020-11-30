a86d005bb8ba682e80e81a163d7b4dcae87a876a
mosquid <mosquid@gmail.com>
2020-11-30 16:08:05 +0200

    bugfix

--- background.js
+++ background.js
@@ -1,35 +1,27 @@
 "use strict"
-function initParasitBehavior() {
-  chrome.tabs.onUpdated.addListener(() => {
-    chrome.storage.sync.get(
-      ["parasites", "redirect"],
-      ({ parasites, redirect }) => {
-        if (!parasites || !parasites.length) return
+chrome.tabs.onUpdated.addListener(() => {
+  chrome.storage.sync.get(
+    ["parasites", "redirect"],
+    ({ parasites, redirect }) => {
+      if (!parasites || !parasites.length) return
 
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            const activeTab = tabs[0]
-            if (!activeTab) return
-            const { url, id } = activeTab
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        const activeTab = tabs[0]
+        if (!activeTab) return
+        const { url, id } = activeTab
 
-            for (let parasite of JSON.parse(parasites)) {
-              if (url.includes(parasite)) {
-                chrome.tabs.executeScript(id, {
-                  code: "window.stop();",
-                  runAt: "document_start",
-                })
-                chrome.tabs.update(id, { url: redirect })
-                break
-              }
-            }
+        for (let parasite of JSON.parse(parasites)) {
+          console.log(parasite)
+          if (url.includes(parasite)) {
+            chrome.tabs.executeScript(id, {
+              code: "window.stop();",
+              runAt: "document_start",
+            })
+            chrome.tabs.update(id, { url: redirect })
+            break
           }
-        )
-      }
-    )
-  })
-}
-
-chrome.runtime.onInstalled.addListener(function () {
-  initParasitBehavior()
+        }
+      })
+    }
+  )
 })
