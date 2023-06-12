dfc40a2b0d91610385cb02878aa0066691d0f8ce
Dmitry Mosquid <mosquid@gmail.com>
2023-06-12 21:15:47 +0300

    redirect fixed

--- background.js
+++ background.js
@@ -1,31 +1,31 @@
 "use strict";
+
 chrome.tabs.onUpdated.addListener(() => {
   chrome.storage.sync.get(
-    ["parasites", "redirect"],
-    ({ parasites, redirect }) => {
-      if (!parasites || !parasites.length) return;
+    ["parasites", "redirect", "isActive"],
+    ({ parasites, redirect, isActive }) => {
+      if (!isActive || !parasites || !parasites.length) return;
 
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        if (!activeTab) return;
-        const { url: currentTabUrl, id } = activeTab;
-        try {
-          const parasiteList = JSON.parse(parasites);
+      chrome.tabs.query(
+        { active: true, currentWindow: true },
+        function named(tabs) {
+          const activeTab = tabs[0];
+          if (!activeTab) return;
+          const { url: currentTabUrl, id } = activeTab;
+          try {
+            const parasiteList = JSON.parse(parasites);
 
-          for (let parasite of parasiteList) {
-            if (currentTabUrl.includes(parasite)) {
-              chrome.tabs.executeScript(id, {
-                code: "window.stop();",
-                runAt: "document_start",
-              });
-              chrome.tabs.update(id, { url: redirect });
-              break;
+            for (let parasite of parasiteList) {
+              if (currentTabUrl.includes(parasite)) {
+                chrome.tabs.update(id, { url: redirect });
+                break;
+              }
             }
+          } catch (error) {
+            console.error(error);
           }
-        } catch (error) {
-          console.error(error);
         }
-      });
+      );
     }
   );
 });
